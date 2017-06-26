fc56937402df9a2348b37994005ae07c57bf4163
Igor Sosa Mayor <joseleopoldo1792@gmail.com>
2017-06-26 23:21:40 +0200

    Quitados varios países: solo 6.

--- code.R
+++ code.R
@@ -1,41 +1,18 @@
-library(tidyverse)
-
-austria <- read.csv("/home/igor/geschichte/projekte/proghistorian/sparql/data/austria.tsv", header = T, sep = "\t")
-belgium <- read.csv("/home/igor/geschichte/projekte/proghistorian/sparql/data/belgium.tsv", header = T, sep = "\t")
+# we read and load all data into variables
 bulgaria <- read.csv("/home/igor/geschichte/projekte/proghistorian/sparql/data/bulgaria.tsv", header = T, sep = "\t")
-croatia <- read.csv("/home/igor/geschichte/projekte/proghistorian/sparql/data/croatia.tsv", header = T, sep = "\t")
-cyprus <- read.csv("/home/igor/geschichte/projekte/proghistorian/sparql/data/cyprus.tsv", header = T, sep = "\t")
-czech <- read.csv("/home/igor/geschichte/projekte/proghistorian/sparql/data/czech.tsv", header = T, sep = "\t")
-denmark <- read.csv("/home/igor/geschichte/projekte/proghistorian/sparql/data/denmark.tsv", header = T, sep = "\t")
-estonia <- read.csv("/home/igor/geschichte/projekte/proghistorian/sparql/data/estonia.tsv", header = T, sep = "\t")
-finland <- read.csv("/home/igor/geschichte/projekte/proghistorian/sparql/data/finland.tsv", header = T, sep = "\t")
 france <- read.csv("/home/igor/geschichte/projekte/proghistorian/sparql/data/france.tsv", header = T, sep = "\t")
 germany <- read.csv("/home/igor/geschichte/projekte/proghistorian/sparql/data/germany.tsv", header = T, sep = "\t")
-#greece <- read.csv("/home/igor/geschichte/projekte/proghistorian/sparql/data/greece.tsv", header = T, sep = "\t")
 hungary <- read.csv("/home/igor/geschichte/projekte/proghistorian/sparql/data/hungary.tsv", header = T, sep = "\t")
-ireland <- read.csv("/home/igor/geschichte/projekte/proghistorian/sparql/data/ireland.tsv", header = T, sep = "\t")
-italy <- read.csv("/home/igor/geschichte/projekte/proghistorian/sparql/data/italy.tsv", header = T, sep = "\t")
-latvia <- read.csv("/home/igor/geschichte/projekte/proghistorian/sparql/data/latvia.tsv", header = T, sep = "\t")
-lithuania <- read.csv("/home/igor/geschichte/projekte/proghistorian/sparql/data/lithuania.tsv", header = T, sep = "\t")
-netherlands <- read.csv("/home/igor/geschichte/projekte/proghistorian/sparql/data/netherlands.tsv", header = T, sep = "\t")
 poland <- read.csv("/home/igor/geschichte/projekte/proghistorian/sparql/data/poland.tsv", header = T, sep = "\t")
 portugal <- read.csv("/home/igor/geschichte/projekte/proghistorian/sparql/data/portugal.tsv", header = T, sep = "\t")
-romania <- read.csv("/home/igor/geschichte/projekte/proghistorian/sparql/data/romania.tsv", header = T, sep = "\t")
-slovakia <- read.csv("/home/igor/geschichte/projekte/proghistorian/sparql/data/slovakia.tsv", header = T, sep = "\t")
-slovenia <- read.csv("/home/igor/geschichte/projekte/proghistorian/sparql/data/slovenia.tsv", header = T, sep = "\t")
-spain <- read.csv("/home/igor/geschichte/projekte/proghistorian/sparql/data/spain.tsv", header = T, sep = "\t")
-sweden <- read.csv("/home/igor/geschichte/projekte/proghistorian/sparql/data/sweden.tsv", header = T, sep = "\t")
-uk <- read.csv("/home/igor/geschichte/projekte/proghistorian/sparql/data/uk.tsv", header = T, sep = "\t")
 
-eudata <- rbind(austria, belgium, bulgaria, croatia, cyprus, czech, denmark,
-                estonia, finland, france, germany, hungary, ireland, italy, latvia,
-                lithuania, netherlands, poland, portugal, romania, slovakia, slovenia,
-                spain, sweden, uk)
+# we create a dataframe with all EU countries
+eudata <- rbind(bulgaria, france, germany,
+                hungary, poland, portugal)
 
-rm(austria, belgium, bulgaria, croatia, cyprus, czech, denmark,
-   estonia, finland, france, germany, hungary, ireland, italy, latvia,
-   lithuania, netherlands, poland, portugal, romania, slovakia, slovenia,
-   spain, sweden, uk)
+# we remove the previous variables
+rm(bulgaria, france, germany,
+   hungary, poland, portugal)
 
 eudata$samecountry <- ifelse(as.character(eudata$origincountry) == as.character(eudata$destination_countryLabel), "same", "different")
 eudata$samecountry <- as.factor(eudata$samecountry)
@@ -43,7 +20,7 @@ eudata$samecountry <- as.factor(eudata$samecountry)
 eudata <- eudata %>% dplyr::mutate(typecountry = case_when(samecountry == "same" & eu == "EU" ~ "same",
                                              samecountry == "different" & eu == "EU" ~ "EU",
                                              samecountry == "different" & eu == "Non-EU" ~ "Non-EU"))
-eudata$typecountry <- factor(eudata$typecountry)
+eudata$connections <- factor(eudata$typecountry)
 
 eudata.percity <- group_by(eudata, origincityLabel) %>% summarise(total = n()) %>% arrange(-total)
 eudata.percity.filtered <- slice(eudata.percity, 1:25)
